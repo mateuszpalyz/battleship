428aa293d58cb89da56f3ae9b83ca7c446ccb544
Mateusz Palyz <mateusz.palyz@netguru.pl>
2016-07-06 11:40:40 +0200

    add actions to game channel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,10 +1,18 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "player_#{uuid}"
+    Match.create(uuid)
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def send_tiles(data)
+    puts data.to_s
+  end
+
+  def shoot(data)
+    Game.make_move(uuid, data)
   end
 end
