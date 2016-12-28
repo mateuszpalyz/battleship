8b400df5213763d3c85b180163e9987e8003e1b8
Mateusz Palyz <mateusz.palyz@netguru.pl>
2016-12-28 22:55:42 +0100

    perform shot in emitter

--- app/react/subscribers/game-emitter.js
+++ app/react/subscribers/game-emitter.js
@@ -1,5 +1,9 @@
 export default App.cable.subscriptions.create('GameChannel', {
   sendTiles(tiles) {
     this.perform('send_tiles', {tiles: tiles});
+  },
+
+  sendShot(tile) {
+    this.perform('shot', {tile: tile});
   }
 })
