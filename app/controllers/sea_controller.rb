37d49426436dcb6846543a270b26159d6d34af84
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-04-03 19:47:03 +0200

    pass firebase config as props to sea component

--- app/controllers/sea_controller.rb
+++ app/controllers/sea_controller.rb
@@ -1,2 +1,18 @@
 class SeaController < ApplicationController
+  def index
+    @props = { firebase_config: firebase_config }
+  end
+
+  private
+
+  def firebase_config
+    {
+      apiKey: Rails.application.secrets.firebase_api_key,
+      authDomain: Rails.application.secrets.firebase_auth_domain,
+      databaseURL: Rails.application.secrets.firebase_database_URL,
+      projectId: Rails.application.secrets.firebase_project_id,
+      storageBucket: Rails.application.secrets.firebase_storage_bucket,
+      messagingSenderId: Rails.application.secrets.firebase_messaging_sender_id
+    }
+  end
 end
