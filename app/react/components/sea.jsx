1c54b08e4dcd90c18302e52f2edc203619119736
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-04-08 14:22:10 +0200

    check for won/lost game, handle opponent shots

--- app/react/components/sea.jsx
+++ app/react/components/sea.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import GameStatus from './game_status';
+import TurnIndicator from './turn_indicator';
 import Tile from './tile';
 import Firebase from 'firebase';
+import board from '../utils/board';
 
 export default class Sea extends React.Component {
   constructor(props) {
@@ -11,20 +13,12 @@ export default class Sea extends React.Component {
     this.database = Firebase.database();
     this.auth.signInAnonymously();
     this.gamesRef = this.database.ref('/games');
-    this.onTileClick = this.onTileClick.bind(this);
     this.onTileBlackClick = this.onTileBlackClick.bind(this);
     this.matchPlayers();
 
-    const board = [
-      '', '', '', '',
-      '', '', '', '',
-      '', '', '', '',
-      'S', 'S', 'S', 'S'
-    ]
-
     this.state = {
-      tiles: this.shuffleTiles(board.slice()),
-      tilesForOpponent: board.slice(),
+      tiles: board.shuffleTiles(),
+      tilesForOpponent: board.emptyLayout(),
       status: 'waiting'
     };
   }
@@ -33,48 +27,101 @@ export default class Sea extends React.Component {
     this.gamesRef.limitToLast(1).once('child_added').then((snapshot) => {
       if (snapshot.val().player2 != null) {
         let newGame = this.gamesRef.push({ player1: this.auth.currentUser.uid });
-        this.player2Ref = this.database.ref(`games/${newGame.key}/player2`);
-        this.player2Ref.on('value', (snapshot)=> {
+        let player2Ref = this.database.ref(`games/${newGame.key}/player2`);
+        player2Ref.on('value', (snapshot)=> {
           if (snapshot.val() != null) this.setState({ status: 'started' });
         });
+        player2Ref.on('child_added', (snapshot)=> {
+          this.updateMyTiles(snapshot.val());
+        });
+        this.setState({gameId: newGame.key, player: 'player1', turn: 'player1'});
       } else {
         this.setState({ status: 'started' });
         var updates = {}
         updates[snapshot.key] = { player1: snapshot.val().player1, player2: this.auth.currentUser.uid };
         this.gamesRef.update(updates);
+        let player1Ref = this.database.ref(`games/${snapshot.key}/player1`);
+        player1Ref.on('child_added', (snapshot)=> {
+          this.updateMyTiles(snapshot.val());
+        });
+        this.setState({gameId: snapshot.key, player: 'player2', turn: 'player1'})
       }
+      this.hitsRef = this.database.ref(`games/${this.state.gameId}/hits`);
+      this.hitsRef.on('child_added', (snapshot)=> {
+        this.updateMyShot(snapshot.val());
+      });
     });
   }
 
-  shuffleTiles(tiles) {
-    for (var i = tiles.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = tiles[i];
-      tiles[i] = tiles[j];
-      tiles[j] = temp;
+  updateMyShot(shot) {
+    if (shot[0] === this.state.player) {
+      let tiles = this.state.tilesForOpponent;
+      tiles[shot[1]] = 'X';
+      this.setState({tilesForOpponent: tiles});
     }
-    return tiles;
+    this.checkIfWon();
   }
 
-  onTileClick(position, player) {
-    if (this.state.status === 'waiting') {
-      alert('Game has not started yet!')
+  updateMyTiles(position) {
+    let tiles = this.state.tiles;
+    if ( (tiles[position] === 'X' || tiles[position] === 'O') ) return;
+    if (tiles[position] === 'S') {
+      tiles[position] = 'X';
+      this.hitsRef.push([this.getOppositePlayer(), position])
     } else {
-      const tiles = this.state.tiles;
-      if ( (tiles[position] === 'X' || tiles[position] === 'I') ) return;
-      if (tiles[position] === 'S') tiles[position] = 'X';
-      this.setState({tiles: tiles, turn: player === 'I' ? 'X' : 'O'});
+      tiles[position] = 'O';
     }
+    this.setState({tiles: tiles, turn: this.state.player});
+    this.checkIfLost();
   }
 
   onTileBlackClick(position, player) {
     if (this.state.status === 'waiting') {
-      alert('Game has not started yet!')
+      alert('Game has not started yet!');
+      return;
+    }
+    if (this.state.turn !== this.state.player) {
+      alert('This is not your turn!');
+      return;
+    }
+    let tiles = this.state.tilesForOpponent;
+    if (tiles[position] === 'X' || tiles[position] === 'O') return;
+    this.database.ref(`/games/${this.state.gameId}/${this.state.player}`).push(position);
+
+    if (tiles[position] === 'S') {
+      tiles[position] = 'X';
     } else {
-      const tiles = this.state.tilesForOpponent;
-      if ( (tiles[position] === 'X' || tiles[position] === 'I') ) return;
-      if (tiles[position] === 'S') tiles[position] = 'X';
-      this.setState({tiles: tiles, turn: player === 'I' ? 'X' : 'O'});
+      tiles[position] = 'O';
+    }
+
+    this.setState({tilesForOpponent: tiles, turn: this.getOppositePlayer()});
+  }
+
+  getOppositePlayer() {
+    if (this.state.player === 'player1') {
+      return 'player2';
+    } else {
+      return 'player1';
+    }
+  }
+
+  checkIfLost() {
+    let sunkenShips = this.state.tiles.filter((t) => {
+      return t === 'X';
+    }).length
+    if (sunkenShips === 4) {
+      this.setState({status: 'lost'});
+      alert('You lose :(');
+    }
+  }
+
+  checkIfWon() {
+    let sunkenShips = this.state.tilesForOpponent.filter((t) => {
+      return t === 'X';
+    }).length
+    if (sunkenShips === 4) {
+      this.setState({status: 'won'});
+      alert('You win :)');
     }
   }
 
@@ -82,6 +129,7 @@ export default class Sea extends React.Component {
     return (
       <div>
         <GameStatus status={this.state.status}/>
+        <TurnIndicator turn={this.state.turn} player={this.state.player}/>
         <div className='game-container'>
           <div className='board'>
             { this.state.tiles.map(function(tile, position) {
