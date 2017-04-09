9bd0da3607b2d3badd647957267e962a31a217e2
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-04-09 16:09:39 +0200

    add more tests

--- app/react/tests/board.test.js
+++ app/react/tests/board.test.js
@@ -17,3 +17,11 @@ test('returns empty array of tiles', ()=> {
     '', '', '', ''
   ]);
 });
+
+test('returns array with four ship present', ()=> {
+  expect(board.shuffleTiles().filter((x)=> { return x === 'S' })).toHaveLength(4);
+});
+
+test('returns array of 16 tiles', ()=> {
+  expect(board.shuffleTiles()).toHaveLength(16);
+});
