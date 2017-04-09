5772dd767a6078fa919394471c9cdba713fb6a1e
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-04-09 13:30:53 +0200

    updatem gems and node packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
-    main: ['./app/react/index.js']
+    main: ['./app/react/index.js'],
   },
   output: {
-    path: __dirname + '/app/assets/javascripts',
-    filename: 'react_bundle.js'
+    path: path.join(__dirname, '/app/assets/javascripts'),
+    filename: 'react_bundle.js',
   },
   module: {
     loaders: [
       {
-        key: 'jsx',
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loaders: ['babel']
+        loaders: ['babel-loader'],
       },
       {
-        key: 'style',
         test: /\.s?css$/,
-        loader: ExtractTextPlugin.extract('css!sass')
-      }
-    ]
+        loader: ExtractTextPlugin.extract('css!sass'),
+      },
+      {
+        test: /\.ya?ml$/,
+        loaders: ['json-loader', 'yaml-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.js.jsx']
+    extensions: ['.js', '.jsx', '.js.jsx'],
   },
   plugins: [
-    new ExtractTextPlugin('../stylesheets/react_bundle.css', {
-      allChunks: true
-    })
-  ]
+    new ExtractTextPlugin({
+      filename: '../stylesheets/react_bundle.css', allChunks: true,
+    }),
+  ],
 };
